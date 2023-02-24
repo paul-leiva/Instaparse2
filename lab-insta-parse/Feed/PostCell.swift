784ab1c3ff737a98b993e49da8ac7f0500763e77
Paul Leiva <54649136+paul-leiva@users.noreply.github.com>
2023-02-24 16:23:39 -0500

    Step 6 Complete

--- lab-insta-parse/Feed/PostCell.swift
+++ lab-insta-parse/Feed/PostCell.swift
@@ -56,7 +56,26 @@ class PostCell: UITableViewCell {
 
         // TODO: Pt 2 - Show/hide blur view
 
-
+        /// A lot of the following returns optional values, so we'll unwrap them all together in one big `if let`
+        /// Get the current user.
+        if let currentUser = User.current,
+           
+            /// Get the date the user last shared a post (cast to Date).
+            let lastPostedDate = currentUser.lastPostedDate,
+        
+                /// Get the date the given post was created.
+                let postCreatedDate = post.createdAt,
+        
+        /// Get the difference in hours between when the given post was created and the current user last posted
+                let diffHours = Calendar.current.dateComponents([.hour], from: postCreatedDate, to: lastPostedDate).hour {
+                    
+                    /// Hide the blur view if the given post was created within 24 hours of current user's last post. (before or after)
+                    blurView.isHidden = abs(diffHours) < 24
+                }
+        else {
+            /// Default to blue if we can't get or compute the dates above for some reason.
+            blurView.isHidden = false
+        }
     }
 
     override func prepareForReuse() {
